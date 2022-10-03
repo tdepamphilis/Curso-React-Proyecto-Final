a73e23422aa945e776f58d17760c5f5fb84a502d
tomas <tdepamphilis@gmail.com>
2022-10-03 14:42:30 -0300

    ADD Router implementation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NavBar from './components/navbar/NavBar';
 import ItemListContainer from './components/ItemListContainer';
 function App() {
   return (
     <div className="App">
-      <NavBar/>
-      <ItemListContainer greeting={'Bienvenido a Bit-Store.'} />
+      <BrowserRouter>
+        <NavBar/>
+        <Routes>
+          <Route exact path="/" element={<ItemListContainer greeting={'Bienvenido a Bit-Store.'} />}/>
+          <Route exact path="/b" element={<ItemListContainer greeting={'Bienvenido a Bit-Store.'} />}/>
+        </Routes>
+      </BrowserRouter>
        
     </div>
   );
